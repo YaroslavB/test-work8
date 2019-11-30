1a3dfc8f47836cf87d9335467e64d2cafb79fe49
Evgeniy Kuvshinov <smpl@itmywork.com>
2019-11-30 17:19:29 +0500

    add front from vue cli

---
+++ backend/_ide_helper_models.php
@@ -0,0 +1,98 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App{
+/**
+ * App\Category
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $_lft
+ * @property int $_rgt
+ * @property int|null $parent_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Kalnoy\Nestedset\Collection|\App\Category[] $children
+ * @property-read int|null $children_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Item[] $items
+ * @property-read int|null $items_count
+ * @property-read \App\Category|null $parent
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Category d()
+ * @method static \Kalnoy\Nestedset\QueryBuilder|\App\Category newModelQuery()
+ * @method static \Kalnoy\Nestedset\QueryBuilder|\App\Category newQuery()
+ * @method static \Kalnoy\Nestedset\QueryBuilder|\App\Category query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Category whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Category whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Category whereLft($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Category whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Category whereParentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Category whereRgt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Category whereUpdatedAt($value)
+ */
+	class Category extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\Item
+ *
+ * @property string $uuid
+ * @property string $image
+ * @property string $name
+ * @property string $description
+ * @property int $category_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Category $category
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Item newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Item newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Item query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Item whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Item whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Item whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Item whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Item whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Item whereUuid($value)
+ */
+	class Item extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+	class User extends \Eloquent {}
+}
+
---
+++ backend/cmd.sh
@@ -0,0 +1,8 @@
+wait-for-it db:3306 -t 60 -- echo "db started"
+composer install
+artisan migrate
+artisan ide-helper:generate -n
+artisan ide-helper:meta -n
+artisan ide-helper:models -n
+artisan ide-helper:eloquent -n
+php -S backend:8080 -t public
