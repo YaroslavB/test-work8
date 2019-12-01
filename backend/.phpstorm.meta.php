cbbd0aae97b4437b628a0c948f1705ef348bc9e1
Evgeniy Kuvshinov <smpl@itmywork.com>
2019-12-01 19:38:59 +0500

    use make

--- backend/.phpstorm.meta.php
+++ backend/.phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-11-30 13:06:20.
+    * Generated on 2019-11-30 22:59:35.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
